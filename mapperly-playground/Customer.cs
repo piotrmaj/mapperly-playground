cdd8b76aff55bb99fb3e748830b291e0e2d85a85
Piotr Maj <piotrmaj17@gmail.com>
2023-10-27 18:14:05 +0200

    Adding simple mapping

---
+++ mapperly-playground/Customer.cs
@@ -0,0 +1,25 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace mapperly_playground
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class CustomerDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    [Mapper]
+    public partial class CustomerMapper
+    {
+        public partial CustomerDto ToDto(Customer customer);
+        public partial Customer ToDomain(CustomerDto dto);
+    }
+}
--- mapperly-playground/Program.cs
+++ mapperly-playground/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var customer = new Customer
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test",
+                Address = "Address 1"
+            };
+            var mapper = new CustomerMapper();
+            var dto = mapper.ToDto(customer);
         }
     }
 }
